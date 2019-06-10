a2d57682062b4a4f6a84982b2dd4d9bdf3dd2310
popoffib <popoffib@rose-hulman.edu>
2019-06-09 22:21:07 -0400

    done with m1 and m2

--- src/m2r_unit_testing.py
+++ src/m2r_unit_testing.py
@@ -8,7 +8,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Mark Hays,
 """
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -69,10 +69,10 @@ def distance(point):
 main()
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #
 # READ the following, asking questions as needed.
-# When you believe that you understading what is says about UNIT TESTING,
+# When you believe that you understanding what is says about UNIT TESTING,
 # mark the above TO-DO as DONE.
 #
 #   In most exercises we will follow the  UNIT TESTING
